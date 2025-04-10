2161a60b7aac8c967de4ed7bb827693a88e4418f
aayushkakkar26 <aayush90793@gmail.com>
2025-04-11 02:13:21 +0530

    resolving token issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,7 +32,7 @@ const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
